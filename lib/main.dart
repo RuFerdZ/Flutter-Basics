666c1a2a96af792e5c8521a5fa89283b0e8f37a2
ruferdz <ge.rusiru@gmail.com>
2020-04-20 23:24:16 +0530

    Simple Hello World

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// main function is the starting point for all Flutter Apps
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
+
+
